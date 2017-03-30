fc05b830691b3604dc8eceb9550f3f26ef2f53bc
phuangpetpk <phuangpetmn@hotmail.com>
2017-03-30 20:20:55 +0700

    text

--- new_todo.php
+++ new_todo.php
@@ -2,14 +2,26 @@
 $link = mysql_connect('localhost','it58160628','it58160628');
 mysql_query("SET NAMES UTF8");
 mysql_select_db('it58160628',$link);
-if($_POST['topic'] != NULL){
-$start = date('Y-m-d H:i:s');
-$topic = addslashes($_POST['topic']);
-$sql = "INSERT INTO todo (start,topic,status) VALUES ('$start','$topic',0);
-       ";
-mysql_query($sql);
+if($_POST['id'] != NULL){
+	$sql = "SELECT *
+        FROM todo
+        WHERE id =".$_POST['id'];
+    $result=mysql_query($sql);
+    $obj = mysql_fetch_object($result);  
+	if($obj->status != 0){
+	$sql="UPDATE todo SET status =0,
+          finish = NULL 
+          WHERE id =".$_POST['id'];
+        mysql_query($sql);
+}else{
+		$finish = date('Y-m-d H:i:s');
+        $sql="UPDATE todo SET status =1,
+          finish ='".$finish. "' 
+          WHERE id =".$_POST['id'];
+         mysql_query($sql);
+	}
 }
-$sql = "SELECT *FROM todo ORDER BY status,id desc ";
+$sql = "SELECT *FROM todo ORDER BY status,id desc";
 $result = mysql_query($sql,$link);
 $html = "<ol>";
 while($obj = mysql_fetch_object($result)){
